bc8877c05dee858d7f37c6d4aea5be5c4daad58b
Anna Kulykovskaya <trishkiev@gmail.com>
2025-07-24 17:10:16 +0200

    django_hw_5 Inline Forms

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -9,12 +9,27 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+class SubtaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at', 'updated_at')
+    list_display = ('short_title', 'status', 'deadline', 'created_at', 'updated_at')
     list_filter = ('status', 'created_at', 'updated_at', 'categories')
     search_fields = ('title', 'description')
     filter_horizontal = ('categories',)
+    inlines = [SubtaskInline]
+
+    def short_title(self, obj):
+        return (obj.title[:10] + '...') if len(obj.title) > 10 else obj.title
+
+    short_title.short_description = 'Title'
+
+    def update_status(self, request, queryset):
+        queryset.update(status='done')
+    actions = [update_status]
 
 
 @admin.register(SubTask)
